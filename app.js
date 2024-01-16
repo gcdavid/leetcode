2384f8bff400fc5a658fc7a6fd46526ff5f08910
gcdavid <gcdavid17@gmail.com>
2024-01-16 07:27:20 -0500

    letter combinations of a phone number

--- app.js
+++ app.js
@@ -244,3 +244,43 @@ mergeIntervals([
   [8, 10],
   [10, 12],
 ]);
+
+var letterCombinations = function (digits) {
+  const alpha = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  const result = [];
+
+  //dfs recursive helper
+  const dfs = (i, digits, slate) => {
+    //base case
+    if (i === digits.length) {
+      result.push(slate.join(""));
+
+      return;
+    }
+
+    //dfs recursive case
+    let chars = alpha[digits[i]];
+
+    for (let char of chars) {
+      slate.push(char);
+      dfs(i + 1, digits, slate);
+      slate.pop();
+    }
+  };
+
+  dfs(0, digits, []);
+
+  return result;
+};
+
+letterCombinations("23");
