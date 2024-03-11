9abe46df727756d76fa49b353e19bb1521d4bf8d
gcdavid <gcdavid17@gmail.com>
2024-03-11 19:16:38 -0400

    Leetcode 5 : Longest Palindromic Substring

--- app.js
+++ app.js
@@ -1577,4 +1577,33 @@ var lengthOfLargestSubstring = function (s) {
   return longestSubstring;
 };
 
+lengthOfLargestSubstring("pwwkew");
+
+var longestPalindrome = function (s) {
+  let longest = "";
+  function isPal(s, left, right) {
+    while (left >= 0 && right <= s.length && s[left] == s[right]) {
+      left--;
+      right++;
+    }
+
+    return s.slice(left + 1, right);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let oddPal = isPal(s, i, i);
+    let evenPal = isPal(s, i, i + 1);
+
+    let longestPal = oddPal.length > evenPal.length ? oddPal : evenPal;
+
+    if (longestPal.length > longest.length) {
+      longest = longestPal;
+    }
+  }
+
+  return longest;
+};
+
+longestPalindrome("babad");
+
 console.log(lengthOfLargestSubstring("pwwkew"));
