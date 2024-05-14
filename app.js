aee913331b5fdeacb8b0cf25e308f706b8fb03dc
gcdavid <gcdavid17@gmail.com>
2024-05-14 12:44:48 -0400

    Leetcode 189 : Rotate array

--- app.js
+++ app.js
@@ -2348,3 +2348,27 @@ var removeElement = function (nums, val) {
   }
   return index;
 };
+
+var reverse = function (arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+};
+
+var rotate = function (nums, k) {
+  let n = nums.length;
+  k %= n;
+
+  //Reverse the entire array
+  reverse(nums, 0, n - 1);
+  //Reverse the first k elements
+  reverse(nums, 0, k - 1);
+  //Reverse the rest of the array
+  reverse(nums, k, n - 1);
+
+  return nums;
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
